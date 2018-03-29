847bc84b56220df21d516be1681a5f25e3893ee3
johngalay <jadgalay@gmail.com>
2018-03-29 22:38:26 +0800

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	
+	
+	public float smoothSpeed = 10f;
+	public Vector3 offset;
+
+	public Vector2 panLimit;
+
+	private Camera mainCamera;
+
+	void Start () {
+		mainCamera = FindObjectOfType<Camera>();
+	}
+	void FixedUpdate() {
+
+		//offset.x = Input.mousePosition.x;
+		//offset.y = Input.mousePosition.y;
+		//Vector3 pointToLook = Camera.ScreenToWorldPoint(Input.mousePosition);
+		//Debug.DrawLine(target.position, pointToLook, Color.red);
+		
+		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.forward, Vector2.zero);
+		float rayLength;
+
+		if(groundPlane.Raycast(cameraRay, out rayLength)) {
+			Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+			Debug.DrawLine(target.position, pointToLook, Color.blue);
+			Vector2 direction = new Vector2(
+				pointToLook.x - transform.position.x,
+				pointToLook.y - transform.position.y
+			);
+			direction.x = Mathf.Clamp(direction.x, -panLimit.x, panLimit.x);
+			direction.y = Mathf.Clamp(direction.y, -panLimit.y, panLimit.y);
+
+			offset = direction;
+		}
+
+		
+
+		offset.z = -5;
+
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPosition;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody myRigidbody;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+	private Camera mainCamera;
+
+	public GunController theGun;
+
+	// Use this for initialization
+	void Start () {
+		// Gets the component type rigidbody attached to the script.
+		myRigidbody = GetComponent<Rigidbody>(); 
+		mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 
+		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0f);
+		moveVelocity = moveInput * moveSpeed;
+
+		pointLook();
+		
+		// left-click
+		if(Input.GetMouseButtonDown(0)) {
+			theGun.isFiring = true;
+		}
+		if(Input.GetMouseButtonUp(0)) {
+			theGun.isFiring = false;
+		}
+	}
+
+	void FixedUpdate () {
+		myRigidbody.velocity = moveVelocity;
+	}
+
+	void pointLook () {
+		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.forward, Vector2.zero);
+		float rayLength;
+
+		if(groundPlane.Raycast(cameraRay, out rayLength)) {
+			Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+			Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+			
+			Vector2 direction = new Vector2(
+				pointToLook.x - transform.position.x,
+				pointToLook.y - transform.position.y
+			);
+
+			transform.up = direction;
+		}
+	}
+}
